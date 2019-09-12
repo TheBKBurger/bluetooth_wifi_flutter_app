a894d65fdb40f2e2c4b00388faf33ed137b277bf
nbcastano <nbcastano@gmail.com>
2019-09-12 15:36:30 -0400

    Implemented: - Changed the colors to black and purple with a white text - Added a bottom navigation bar. Tested on iPhone Xr and Samsung Note 9, both look good.
    Under the hood:
    - Cleaned up some code to make it easier to read by pulling out the bottom nav bar and the button row into their own methods.
    - In order to properly change the icon and text color at the bottom nav bar, added some IconTheme and Text Theme. Hoping to find an easier way later.

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
-
+        primarySwatch: Colors.purple,
       ),
-      home: MyHomePage(title: 'Bluetooth & WiFi Info App'),
+      home: MyHomePage(title: 'Bluetooth & WiFi Data Collection'),
     );
   }
 }
@@ -45,91 +44,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+//  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      //This can be the method used to share the logs created with the app. Maybe have it open to a share window.
-
-
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+//  void _incrementCounter() {
+//    setState(() {
+//      //This can be the method used to share the logs created with the app. Maybe have it open to a share window.
+//
+//      // This call to setState tells the Flutter framework that something has
+//      // changed in this State, which causes it to rerun the build method below
+//      // so that the display can reflect the updated values. If we changed
+//      // _counter without calling setState(), then the build method would not be
+//      // called again, and so nothing would appear to happen.
+//      _counter++;
+//    });
+//  }
 
   //This is where the Bluetooth and WiFi logs would be collected and start.
-  void _startBluetoothLogging(){
-    //This is where the Bluetooth and WiFi logs would be collected and start.
-  }
-  void _startWiFILogging(){
+  void _startBluetoothLogging() {}
 
-  }
+  void _startWiFILogging() {}
 
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
 
-    var scaffold = Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Container(
-
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'This is an application created to collect Bluetooth data during stress testing. Please ensure the phone will not turn off the display or go to sleep at any time.',
-            textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 15),
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            //THIS IS THE BOTTOM BUTTONS
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                RaisedButton.icon(
-                  onPressed: _startBluetoothLogging,
-                  icon: Icon(Icons.bluetooth),
-                  label: Text('Start Bluetooth Logging'),
-                ),
-                RaisedButton.icon(
-                  onPressed: _startWiFILogging,
-                  icon: Icon(Icons.wifi),
-                  label: Text('Start WiFi Logging'),
-                ),
-              ],
-            ),
-            //END BOTTOM BUTTONS
-          ],
+    var buttonRow = new Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        RaisedButton.icon(
+          onPressed: _startBluetoothLogging,
+          icon: Icon(Icons.bluetooth),
+          label: Text('Start Bluetooth Logging'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Share Logs',
-        child: Icon(Icons.mail),
-      ),
-
+        RaisedButton.icon(
+          onPressed: _startWiFILogging,
+          icon: Icon(Icons.wifi),
+          label: Text('Start WiFi Logging'),
+        ),
+      ],
     );
+
+    var bottomNavBar = new BottomNavigationBar(
+      backgroundColor: Colors.purple,
+      currentIndex: 0,
+      items: [
+        BottomNavigationBarItem(
+            icon: IconTheme(
+                data: IconThemeData(color: Colors.white),
+                child: Icon(Icons.home)),
+                title: Text('Home', style: new TextStyle(color: Colors.white),),
+        ),
+        BottomNavigationBarItem(
+            icon: IconTheme(
+                data: IconThemeData(color: Colors.white),
+                child: Icon(Icons.settings)),
+                title: Text('Settings', style: new TextStyle(color: Colors.white),),
+        ),
+
+      ],
+    );
+
+    var scaffold = Scaffold(
+        backgroundColor: Colors.white24,
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'This is an application created to collect Bluetooth data during stress testing. Please ensure the phone will not turn off the display or go to sleep at any time.',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 15),
+              ),
+              buttonRow,
+            ],
+          ),
+        ),
+        bottomNavigationBar: bottomNavBar);
     return scaffold;
   }
 }
